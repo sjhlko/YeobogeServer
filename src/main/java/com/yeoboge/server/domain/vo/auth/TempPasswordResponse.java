6a90ee4f64503d6bc8d5280dba797dcf0d4e9211
sjhlko444 <sjhlko444@gmail.com>
2023-07-28 03:15:36 +0900

    🔥 MessageResponse로 대체된 vo 삭제

--- src/main/java/com/yeoboge/server/domain/vo/auth/TempPasswordResponse.java
+++
@@ -1,6 +0,0 @@
-package com.yeoboge.server.domain.vo.auth;
-
-import lombok.Builder;
-
-@Builder
-public record TempPasswordResponse(String message) { }
--- src/main/java/com/yeoboge/server/domain/vo/auth/UpdatePasswordResponse.java
+++
@@ -1,6 +0,0 @@
-package com.yeoboge.server.domain.vo.auth;
-
-import lombok.Builder;
-
-@Builder
-public record UpdatePasswordResponse(String message) { }

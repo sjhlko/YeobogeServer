614b0bc55ad6e15ac6eb129b20d4b92cce4800fc
s0o0bn <0921soobin@gmail.com>
2023-07-28 22:53:38 +0900

    💡 로깅 관련 클래스 주석 작성

--- src/main/java/com/yeoboge/server/utils/LogFormatUtils.java
+++ src/main/java/com/yeoboge/server/utils/LogFormatUtils.java
@@ -10,6 +10,11 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Http 요청 및 응답 관련 로그를 저장하기 위해 상세 정보를 포맷에 맞춰 생성 후 반환하는 Util 클래스
+ *
+ * @author Yoon Soobin
+ */
 public class LogFormatUtils {
     private static final String ANONYMOUS_USER = "anonymousUser";
     private static final String DATE_FORMAT = "[dd/MM/yyyy:HH:mm:ss Z]";
@@ -26,6 +31,13 @@ public class LogFormatUtils {
             "IPV6_ADR"
     };
 
+    /**
+     * <p>현재 요청과 응답에 대해 로깅할 정보를 문자열로 파싱하여 반환함.
+     *
+     * @param request 현재 Http request
+     * @param response 현재 요청에 대한 Http response
+     * @return 로깅할 데이터를 포맷에 맞게 변환한 문자열
+     */
     public static String getHttpLog(HttpServletRequest request, HttpServletResponse response) {
         return getRemoteAddr(request) + " "
                 + getRemoteUser() + " "
@@ -36,6 +48,12 @@ public class LogFormatUtils {
                 + response.getStatus();
     }
 
+    /**
+     * 요청을 보낸 Host의 IP 주소를 파싱하기 위해 Request Header에서 주소값을 가져옴
+     *
+     * @param request 현재 Http request
+     * @return 클라이언트 IP 주소
+     */
     private static String getRemoteAddr(HttpServletRequest request) {
         List<String> remoteHostHeaders = List.of(REMOTE_HOST_HEADERS);
 
@@ -48,11 +66,22 @@ public class LogFormatUtils {
         return request.getRemoteAddr();
     }
 
+    /**
+     * <p>현재 요청을 보낸 사용자가 로그인한 사용자라면 해당 사용자의 ID를 반환하고,</p>
+     * 로그인 정보가 없다면 익명의 사용자라는 디폴트 값을 반환함.
+     *
+     * @return 현재 요청을 보낸 사용자
+     */
     private static String getRemoteUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return auth != null ? String.valueOf(auth.getPrincipal()) : ANONYMOUS_USER;
     }
 
+    /**
+     * 현재 요청을 보낸 시각을 포맷에 맞게 문자열로 변환하여 반환함.
+     *
+     * @return 요청을 보낸 시각
+     */
     private static String getDateTime() {
         Date now = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);

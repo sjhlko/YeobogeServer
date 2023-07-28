4fb07222b936674f6aaff966b6cb12cda16da674
s0o0bn <0921soobin@gmail.com>
2023-07-28 20:22:52 +0900

    🐛 클라이언트 IP 주소 파싱 시 헤더 우선 확인으로 수정

--- src/main/java/com/yeoboge/server/utils/LogFormatUtils.java
+++ src/main/java/com/yeoboge/server/utils/LogFormatUtils.java
@@ -4,13 +4,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class LogFormatUtils {
     private static final String ANONYMOUS_USER = "anonymousUser";
     private static final String DATE_FORMAT = "[dd/MM/yyyy:HH:mm:ss Z]";
+    private static final String[] REMOTE_HOST_HEADERS = {
+            "X-Forwarded-For",
+            "HTTP_CLIENT_IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_VIA",
+            "IPV6_ADR"
+    };
 
     public static String getHttpLog(HttpServletRequest request, HttpServletResponse response) {
         return getRemoteAddr(request) + " "
@@ -23,6 +37,14 @@ public class LogFormatUtils {
     }
 
     private static String getRemoteAddr(HttpServletRequest request) {
+        List<String> remoteHostHeaders = List.of(REMOTE_HOST_HEADERS);
+
+        for (String header : remoteHostHeaders) {
+            String clientIp = request.getHeader(header);
+            if (StringUtils.hasText(clientIp) && !clientIp.equals("unknown"))
+                return clientIp;
+        }
+
         return request.getRemoteAddr();
     }
 

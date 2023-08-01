6d43f094f1a7ebdf6efa60a5f9c69c3d372a4d5f
s0o0bn <0921soobin@gmail.com>
2023-08-01 15:39:29 +0900

    💡 Spring Security 관련 코드 JavaDoc 작성

--- src/main/java/com/yeoboge/server/config/security/HttpEndpointChecker.java
+++ src/main/java/com/yeoboge/server/config/security/HttpEndpointChecker.java
@@ -8,11 +8,22 @@ import org.springframework.web.servlet.HandlerExecutionChain;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
+/**
+ * 현재 서버로 들어온 Http 요청을 처리하는
+ * {@link DispatcherServlet} 핸들러가 존재하는 지 확인하는 Component
+ */
 @Component
 @RequiredArgsConstructor
 public class HttpEndpointChecker {
     private final DispatcherServlet servlet;
 
+    /**
+     * 전체 {@link DispatcherServlet} 핸들러에 대해
+     * 현재 요청을 처리하는 핸들러가 존재하는 지 확인함.
+     *
+     * @param request 현재 Http 요청
+     * @return 핸들러가 존재하면 true, 아니면 false
+     */
     public boolean isEndpointExist(HttpServletRequest request) {
         for (HandlerMapping mapping : servlet.getHandlerMappings()) {
             try {

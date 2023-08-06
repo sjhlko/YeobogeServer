773d52108be2740d2ee1da3eefc4914a25c91e52
s0o0bn <0921soobin@gmail.com>
2023-08-06 17:16:55 +0900

    ✨ 보드게임 평가 및 찜하기에 생성 날짜 컬럼 추가

--- src/main/java/com/yeoboge/server/domain/entity/BookmarkedBoardGame.java
+++ src/main/java/com/yeoboge/server/domain/entity/BookmarkedBoardGame.java
@@ -4,10 +4,15 @@ package com.yeoboge.server.domain.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class BookmarkedBoardGame {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +26,10 @@ public class BookmarkedBoardGame {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_game_id")
     private BoardGame boardGame;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime bookmarked;
 
     public void setParent(User user, BoardGame boardGame) {
         this.user = user;

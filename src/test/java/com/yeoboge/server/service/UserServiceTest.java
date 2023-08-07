f6601c3ff776f6b64a604e55581ce195dee376fc
sjhlko444 <sjhlko444@gmail.com>
2023-08-07 18:09:43 +0900

    ✅ 프로필 변경 성공 단위 테스트 추가

--- src/test/java/com/yeoboge/server/service/UserServiceTest.java
+++ src/test/java/com/yeoboge/server/service/UserServiceTest.java
@@ -1,7 +1,9 @@
 package com.yeoboge.server.service;
 
 import com.yeoboge.server.domain.dto.user.UserDetailResponse;
+import com.yeoboge.server.domain.dto.user.UserUpdateRequest;
 import com.yeoboge.server.domain.entity.User;
+import com.yeoboge.server.domain.vo.response.MessageResponse;
 import com.yeoboge.server.enums.error.AuthenticationErrorCode;
 import com.yeoboge.server.handler.AppException;
 import com.yeoboge.server.repository.UserRepository;
@@ -12,10 +14,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,6 +30,8 @@ class UserServiceTest {
     private UserServiceImpl userService;
     @Mock
     private UserRepository userRepository;
+    @Mock
+    private S3FileUploadService s3FileUploadService;
 
     @Test
     @DisplayName("회원 프로필 조회_성공")
@@ -42,7 +50,7 @@ class UserServiceTest {
 
     @Test
     @DisplayName("회원 프로필 조회_실패 : 회원이 존재하지 않음")
-    void getProfileFailed1() {
+    void getProfileFailed() {
         // given
         User user = User.builder()
                 .id(0L)
@@ -59,6 +67,76 @@ class UserServiceTest {
 
 
     @Test
-    void updateUser() {
+    @DisplayName("회원 프로필 변경_성공 : 프로필 사진 변경")
+    void updateUserSuccess1() {
+        // given
+        User user = User.builder()
+                .id(0L)
+                .build();
+        UserUpdateRequest request = UserUpdateRequest.builder()
+                .isChanged(true)
+                .nickname("변경")
+                .build();
+        MockMultipartFile file = new MockMultipartFile("file",
+                "test.img", "png",
+                "test file".getBytes(StandardCharsets.UTF_8) );
+        String imgUrl = "url";
+
+        // when
+        when(userRepository.getById(user.getId())).thenReturn(user);
+        when(s3FileUploadService.uploadFile(any())).thenReturn(imgUrl);
+        when(userRepository.save(any())).thenReturn(user);
+
+        // then
+        assertThat(userService.updateUser(file,request,user.getId()))
+                .isEqualTo(MessageResponse.builder()
+                        .message("프로필 변경 성공")
+                        .build());
+    }
+
+    @Test
+    @DisplayName("회원 프로필 변경_성공 : 프로필 사진 유지")
+    void updateUserSuccess2() {
+        // given
+        User user = User.builder()
+                .id(0L)
+                .build();
+        UserUpdateRequest request = UserUpdateRequest.builder()
+                .isChanged(false)
+                .nickname("변경")
+                .build();
+
+        // when
+        when(userRepository.getById(user.getId())).thenReturn(user);
+        when(userRepository.save(any())).thenReturn(user);
+
+        // then
+        assertThat(userService.updateUser(null,request,user.getId()))
+                .isEqualTo(MessageResponse.builder()
+                        .message("프로필 변경 성공")
+                        .build());
+    }
+
+    @Test
+    @DisplayName("회원 프로필 변경_성공 : 프로필 사진 삭제")
+    void updateUserSuccess23() {
+        // given
+        User user = User.builder()
+                .id(0L)
+                .build();
+        UserUpdateRequest request = UserUpdateRequest.builder()
+                .isChanged(true)
+                .nickname("변경")
+                .build();
+
+        // when
+        when(userRepository.getById(user.getId())).thenReturn(user);
+        when(userRepository.save(any())).thenReturn(user);
+
+        // then
+        assertThat(userService.updateUser(null,request,user.getId()))
+                .isEqualTo(MessageResponse.builder()
+                        .message("프로필 변경 성공")
+                        .build());
     }
 }

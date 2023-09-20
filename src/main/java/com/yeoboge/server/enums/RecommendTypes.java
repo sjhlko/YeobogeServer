81c9e1e6d863ebfa6bc3ab811c66a1a29b89ae09
s0o0bn <0921soobin@gmail.com>
2023-09-20 23:09:41 +0900

    🐛 개인 추천 카테고리 타입 Enum 수정

--- src/main/java/com/yeoboge/server/enums/RecommendTypes.java
+++ src/main/java/com/yeoboge/server/enums/RecommendTypes.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum RecommendTypes {
     TOP_10("top10"),
-    PERSONAL_RECOMMEND("personalRec"),
     MY_BOOKMARK("bookmark"),
     FAVORITE_GENRE("favoriteGenre"),
     FRIENDS_FAVORITE("friendsFavorite");

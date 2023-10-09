ba08f9dbd5596f519e98d5b71e47fe1bd7c99ab2
s0o0bn <0921soobin@gmail.com>
2023-10-09 13:21:11 +0900

    💡 JavaDoc 오타 수정

--- src/main/java/com/yeoboge/server/helper/recommender/RecommendedBySQL.java
+++ src/main/java/com/yeoboge/server/helper/recommender/RecommendedBySQL.java
@@ -23,7 +23,7 @@ public abstract class RecommendedBySQL extends RecommendedBySomethingBase {
      * 외부 API를 요청하는 로직이 비동기로 작업하므로 DB에서 추천 목록을 조회하는 로직도 비동기 동작하도록 설정하고,
      * 비동기로 받은 조회 결과를 {@link RecommendForSingleResponse}에 추가함.
      *
-     * @param response 추천 목록 관련 메타데이터를 담기 위한 {@link RecommendForSingleResponseㄹ직}
+     * @param response 추천 목록 관련 메타데이터를 담기 위한 {@link RecommendForSingleResponse}
      * @param latch 비동기 로직 완료를 기다리기 위한 {@link CountDownLatch}
      */
     protected void setAsyncProcessing(RecommendForSingleResponse response, CountDownLatch latch) {

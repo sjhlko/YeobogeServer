f3c34a7db92885c07cf509fef58d163ebefa8083
s0o0bn <0921soobin@gmail.com>
2023-10-14 00:01:04 +0900

    ✅ 그룹 추천 관련 테스트 코드 추가

--- src/test/java/com/yeoboge/server/service/GroupRecommenderServiceTest.java
+++ src/test/java/com/yeoboge/server/service/GroupRecommenderServiceTest.java
@@ -1,11 +1,14 @@
 package com.yeoboge.server.service;
 
+import com.yeoboge.server.domain.dto.boardGame.BoardGameDetailedThumbnailDto;
 import com.yeoboge.server.domain.dto.recommend.GroupMembersResponse;
 import com.yeoboge.server.domain.dto.recommend.GroupRecommendationResponse;
 import com.yeoboge.server.domain.dto.recommend.UserGpsDto;
 import com.yeoboge.server.domain.dto.user.UserInfoDto;
+import com.yeoboge.server.domain.entity.Genre;
 import com.yeoboge.server.domain.entity.User;
 import com.yeoboge.server.domain.vo.recommend.GroupRecommendationRequest;
+import com.yeoboge.server.domain.vo.recommend.RecommendWebClientResponse;
 import com.yeoboge.server.enums.error.GroupErrorCode;
 import com.yeoboge.server.handler.AppException;
 import com.yeoboge.server.repository.FriendRepository;
@@ -16,9 +19,14 @@ import com.yeoboge.server.service.impl.GroupRecommenderServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,7 +38,7 @@ import java.util.concurrent.Executors;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class GroupRecommenderServiceTest {
@@ -45,11 +53,13 @@ public class GroupRecommenderServiceTest {
     private FriendRepository friendRepository;
     @Mock
     private RecommendRepository recommendRepository;
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private WebClient webClient;
 
     private UserGpsDto gpsDto;
-    private GroupMembersResponse groupResponse;
     private GroupRecommendationRequest request;
     private GroupRecommendationResponse recommendationResponse;
+    private List<BoardGameDetailedThumbnailDto> thumbnails;
     private List<Long> recommendableMembers;
 
     @Test
@@ -122,8 +132,69 @@ public class GroupRecommenderServiceTest {
                 .hasMessageContaining(GroupErrorCode.OVER_LIMIT_GROUP_MEMBER.getMessage());
     }
 
+    @Test
+    @DisplayName("그룹 추천 성공 : AI 추천")
+    public void groupRecommendationSuccessByAI() {
+        // given
+        setUpGroupRecommendationTest();
+        List<Long> recommendedByAi = new ArrayList<>(Collections.nCopies(10, 1L));
+        Mono<RecommendWebClientResponse> monoResponse = Mono.just(
+                new RecommendWebClientResponse(recommendedByAi)
+        );
+
+        // when
+        when(webClient.method(HttpMethod.POST)
+                .uri(anyString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(any())
+                .retrieve()
+                .onStatus(any(), any())
+                .onStatus(any(), any())
+                .bodyToMono(RecommendWebClientResponse.class)
+        ).thenReturn(monoResponse);
+        when(ratingRepository.countByUser(anyLong())).thenReturn(15L);
+        when(recommendRepository.getRecommendedBoardGamesForGroup(recommendedByAi)).thenReturn(thumbnails);
+
+        // then
+        GroupRecommendationResponse actual = groupRecommenderService.getGroupRecommendation(request);
+        assertThat(actual.recommendations()).isEqualTo(recommendationResponse.recommendations());
+        verify(recommendRepository, never()).getPopularBoardGamesOfGenreForGroup(anyLong());
+    }
+
+    @Test
+    @DisplayName("그룹 추천 성공 : 단순 장르 인기순 추천")
+    public void groupRecommendationSuccessByDB() {
+        // given
+        setUpGroupRecommendationTest();
+        List<Genre> favoriteGenres = List.of(
+                Genre.builder().id(1L).name("Strategy").build(),
+                Genre.builder().id(2L).name("Abstract").build(),
+                Genre.builder().id(3L).name("Party").build()
+        );
+
+        // when
+        when(ratingRepository.countByUser(anyLong())).thenReturn(5L);
+        when(recommendRepository.getMyFavoriteGenre(anyLong())).thenReturn(favoriteGenres);
+        when(recommendRepository.getPopularBoardGamesOfGenreForGroup(anyLong())).thenReturn(thumbnails);
+
+        // then
+        GroupRecommendationResponse actual = groupRecommenderService.getGroupRecommendation(request);
+        assertThat(actual.recommendations()).isEqualTo(recommendationResponse.recommendations());
+        verify(recommendRepository, never()).getRecommendedBoardGamesForGroup(anyList());
+    }
 
     private void setUpGroupMatchTest() {
         gpsDto = new UserGpsDto(37.60, 127.09);
+    }
+
+    private void setUpGroupRecommendationTest() {
+        recommendableMembers = new ArrayList<>(List.of(1L, 2L, 3L));
+        request = new GroupRecommendationRequest(recommendableMembers, 1);
+
+        BoardGameDetailedThumbnailDto dto = new BoardGameDetailedThumbnailDto(
+                1L, "name", "image", 2, 5, "1", List.of("genre")
+        );
+        thumbnails = new ArrayList<>(Collections.nCopies(10, dto));
+        recommendationResponse = new GroupRecommendationResponse(thumbnails);
     }
 }

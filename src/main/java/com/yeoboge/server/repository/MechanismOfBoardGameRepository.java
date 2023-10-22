e089b7c11ed04af46576bff12115442aa59548b0
s0o0bn <0921soobin@gmail.com>
2023-10-22 18:35:04 +0900

    🔥 보드게임 DB 저장 관련 코드 삭제

--- src/main/java/com/yeoboge/server/repository/MechanismOfBoardGameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yeoboge.server.repository;
-
-import com.yeoboge.server.domain.entity.MechanismOfBoardGame;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MechanismOfBoardGameRepository extends JpaRepository<MechanismOfBoardGame, Long> {
-}

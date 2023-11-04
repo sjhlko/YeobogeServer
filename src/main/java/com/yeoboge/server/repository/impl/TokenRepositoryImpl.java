fc051ae32b654268733037e2a50a9276b24884a7
s0o0bn <0921soobin@gmail.com>
2023-11-04 21:44:25 +0900

    ✨ 로그인 시 생성되는 사용자의 유효한 Access Token, Refresh Token을 Redis에서 관리하기 위한 메서드 추가

--- src/main/java/com/yeoboge/server/repository/impl/TokenRepositoryImpl.java
+++ src/main/java/com/yeoboge/server/repository/impl/TokenRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.yeoboge.server.enums.error.AuthenticationErrorCode;
 import com.yeoboge.server.handler.AppException;
 import com.yeoboge.server.repository.TokenRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
@@ -20,6 +21,7 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class TokenRepositoryImpl implements TokenRepository {
     private static final Long TIME_TO_LIVE = 60L * 60 * 24 * 7;
+    private static final String AUTH_TOKEN_PREFIX = "TOKENS_";
     private final RedisTemplate redisTemplate;
 
     @Override
@@ -45,6 +47,39 @@ public class TokenRepositoryImpl implements TokenRepository {
     }
 
     @Override
+    public void saveValidTokens(Tokens tokens, long id) {
+        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
+        String key = getCurrentTokenKey(id);
+
+        hashOperations.put(key, "access_token", tokens.accessToken());
+        hashOperations.put(key, "refresh_token", tokens.refreshToken());
+        redisTemplate.expire(key, TIME_TO_LIVE, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void deleteValidTokens(long id) {
+        String key = getCurrentTokenKey(id);
+        if (Boolean.FALSE.equals(redisTemplate.delete(key)))
+            throw new AppException(AuthenticationErrorCode.USER_NOT_FOUND);
+    }
+
+    @Override
+    public Optional<Tokens> findValidTokens(long id) {
+        String key = getCurrentTokenKey(id);
+        String accessToken = (String) redisTemplate.opsForHash().get(key, "access_token");
+        String refreshToken = (String) redisTemplate.opsForHash().get(key, "refresh_token");
+
+        if (Objects.isNull(accessToken)) return Optional.empty();
+
+        return Optional.ofNullable(
+                Tokens.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build()
+        );
+    }
+
+    @Override
     public void saveFcmToken(final Long id, final String token) {
         ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
         valueOperations.set(id.toString(), token);
@@ -61,5 +96,15 @@ public class TokenRepositoryImpl implements TokenRepository {
         String refreshToken = (String) redisTemplate.opsForValue().get(id.toString());
         if (Objects.isNull(refreshToken)) return Optional.empty();
         return Optional.of(refreshToken);
+    }
+
+    /**
+     * 사용자의 유효한 Token을 저장하기 위한 Redis Hash Key를 생성함.
+     *
+     * @param id 사용자 ID
+     * @return 해당 사용자의 Token 저장 Redis Hash Key
+     */
+    private String getCurrentTokenKey(long id) {
+        return String.format("%s%d", AUTH_TOKEN_PREFIX, id);
     }
 }

f6a03150cc44fdc58b85e47cdd8c23c2bd06e5bd
sjhlko444 <sjhlko444@gmail.com>
2023-11-06 07:43:56 +0900

    ✨ STOMP 적용

---
+++ src/main/java/com/yeoboge/server/controller/StompController.java
@@ -0,0 +1,22 @@
+package com.yeoboge.server.controller;
+
+import com.yeoboge.server.domain.vo.chat.StompMessageRequest;
+import com.yeoboge.server.service.StompService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class StompController {
+    private final StompService stompService;
+
+    @CrossOrigin
+    @MessageMapping("/send-message/{targetUserId}")
+    public void test(StompMessageRequest request, @DestinationVariable Long targetUserId, SimpMessageHeaderAccessor accessor) {
+        stompService.sendMessage(accessor, targetUserId, request);
+    }
+}
